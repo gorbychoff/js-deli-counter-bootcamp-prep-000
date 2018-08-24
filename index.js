13a351f23eeb4cccebab2e490e0bd5619aab8fe0
Viam Mercer <viam.mercer@hotmail.com>
2018-08-24 14:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine) {
 function currentLine(line) {
   var i = line.length;
   while (i > 0) {
-    return `The line is currently: ${line[0]}`;
+    return `The line is currently: ${line}`;
   }
   return "The line is currently empty."
 }
