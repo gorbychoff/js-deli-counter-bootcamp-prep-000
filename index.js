99a3744efadebd27240e7694fc4d85db57a4ee8c
Viam Mercer <viam.mercer@hotmail.com>
2018-08-24 13:52:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function takeANumber(currentLine, name) {
   var lineNumber = (currentLine.length + 1);
+  currentLine.push(name);
   return `Welcome, ${name}. You are number ${lineNumber} in line.`;
 }
 
